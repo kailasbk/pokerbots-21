b8062634370fc4c2b539b90dacb7ae2c35e4a687
Kailas Kahler <kailasbk230@gmail.com>
2021-01-08 23:22:42 -0500

    Fixing raising bugs

--- player.py
+++ player.py
@@ -90,6 +90,7 @@ class Player(Bot):
 		'''
 		legal_actions = round_state.legal_actions()
 		my_actions = [None] * NUM_BOARDS
+		stack = round_state.stacks[active]
 		for i in range(NUM_BOARDS):
 			if AssignAction in legal_actions[i]:
 				cards = [self.cards[2*i], self.cards[2*i+1]]
@@ -99,31 +100,43 @@ class Player(Bot):
 				if isinstance(board_state, TerminalState):
 					my_actions[i] = CheckAction()
 					continue
-				stack = round_state.stacks[active]
 				continue_cost = board_state.pips[1-active] - board_state.pips[active]
-				pot = board_state.pot;
+				pot = board_state.pot
 				potOdds = float(continue_cost) / (pot + continue_cost)
 				winProb = self.win_probability(board_state, i, active)
 				
 				# if they haven't raised and we can raise
-				if winProb >= .8 and RaiseAction in legal_actions[i]:
-					if BIG_BLIND <= 3 * BIG_BLIND and 3 * BIG_BLIND <= stack:
-						my_actions[i] = RaiseAction(3 * BIG_BLIND)
+				if winProb >= .8 and RaiseAction in legal_actions[i] and stack != 0:
+					increase = (3 * BIG_BLIND)
+					amount = board_state.pips[1-active] + increase
+					# if can bet normally
+					if stack > 3 * BIG_BLIND + continue_cost and board_state.pips[active] < 30:
+						print('Betting/raising on board {} to {}.'.format(i + 1, amount))
+						my_actions[i] = RaiseAction(amount)
+						stack -= amount
+					# otherwise (don't do all in rn)
 					else:
-						my_actions[i] = RaiseAction(stack)
+						if CallAction in legal_actions[i]:
+							print('Calling on board {}.'.format(i + 1))
+							my_actions[i] = CallAction()
+						elif CheckAction in legal_actions[i]:
+							print('Checking on board {}.'.format(i + 1))
+							my_actions[i] = CheckAction()
 				# if opponent just raised (call and fold, maybe raise, are legal)
 				elif CallAction in legal_actions[i]:
 					# check on the board
 					if (winProb > potOdds):
+						print('Calling on board {}.'.format(i + 1))
 						my_actions[i] = CallAction()
 					else:
-						# change to fold when possible
+						print('Folding on board {}.'.format(i + 1))
 						my_actions[i] = FoldAction()
 				# if its not worth raising, just check
 				elif CheckAction in legal_actions[i]:
 					# else check on the board
+					print('Checking on board {}.'.format(i + 1))
 					my_actions[i] = CheckAction()
-		return my_actions;
+		return my_actions
 
 if __name__ == '__main__':
 	run_bot(Player(), parse_args())
